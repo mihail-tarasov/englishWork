815bdac946880157f51e00cb31fd8e23df39e3d1
mihail-tarasov <mihail.tarasov7@yandex.ru>
2025-08-02 23:15:27 +0300

    первый коммит

---
+++ src/test/java/ru/mihail/tarasov/englishWork/EnglishWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mihail.tarasov.englishWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
