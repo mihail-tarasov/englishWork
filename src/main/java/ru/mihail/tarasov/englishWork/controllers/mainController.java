55e95112d08bf37db8faa0aa4f34cf44660b960b
mihail-tarasov <mihail.tarasov7@yandex.ru>
2025-08-04 14:54:09 +0300

    добавил главный контроллер

---
+++ src/main/java/ru/mihail/tarasov/englishWork/controllers/mainController.java
@@ -0,0 +1,14 @@
+package ru.mihail.tarasov.englishWork.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class mainController {
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("name",name);
+        return "home";
+    }
+}
