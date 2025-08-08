076c0ef1693d59aa029c5ce2d9b8d121c0a4b7c4
mihail-tarasov <mihail.tarasov7@yandex.ru>
2025-08-09 00:27:38 +0300

    без коментариев

---
+++ src/main/java/ru/mihail/tarasov/englishWork/EnglishWorkApplication.java
@@ -0,0 +1,13 @@
+package ru.mihail.tarasov.englishWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishWorkApplication.class, args);
+	}
+
+}
